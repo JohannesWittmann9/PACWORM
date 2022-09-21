0953760672f55ad3c9e086896944a048e7780313
Johannes Wittmann <johannes.wittmann1@gmx.de>
2022-09-22 03:37:48 +1000

    Add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,26 +9,62 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AudioClip playerMovementClip;
 
     private AudioSource audioSource;
+    private Animator animator;
     private Tweener tweener;
+    private bool coroutineRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         tweener = GetComponent<Tweener>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float time = 5 * movementSpeed;
-        Vector3 end1 = transform.position + new Vector3(-5.0f, 0.0f, 0.0f);
-        tweener.AddTween(transform, transform.position, end1, 1.5f);
-        time = 4 * movementSpeed;
-        Vector3 end2 = transform.position + new Vector3(-5.0f, 4.0f, 0.0f);
-        tweener.AddTween(transform, transform.position, end2, time);
-
+    void Update(){
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = playerMovementClip;
+            audioSource.Play();
+        }
+        if (!coroutineRunning)
+        {
+            coroutineRunning = true;
+            StartCoroutine(MoveClockwise());
+        }
     }
 
-
+    IEnumerator MoveClockwise()
+    {
+        float time = 5 * movementSpeed;
+        Vector3 position = transform.position;
+        position.x -= 5;
+        tweener.AddTween(transform, transform.position, position, time);
+        yield return new WaitForSeconds(time);
+        time = 4 * movementSpeed;
+        position = transform.position;
+        position.y += 4;
+        tweener.AddTween(transform, transform.position, position, time);
+        animator.SetFloat("horizontal", 0.0f);
+        animator.SetFloat("vertical", 1.0f);
+        yield return new WaitForSeconds(time);
+        time = 5 * movementSpeed;
+        position = transform.position;
+        position.x += 5;
+        tweener.AddTween(transform, transform.position, position, time);
+        animator.SetFloat("vertical", 0.0f);
+        animator.SetFloat("horizontal", 1.0f);
+        yield return new WaitForSeconds(time);
+        time = 4 * movementSpeed;
+        position = transform.position;
+        position.y -= 4;
+        tweener.AddTween(transform, transform.position, position, time);
+        animator.SetFloat("horizontal", 0.0f);
+        animator.SetFloat("vertical", -1.0f);
+        yield return new WaitForSeconds(time);
+        coroutineRunning = false;
+        animator.SetFloat("horizontal", -1.0f);
+        animator.SetFloat("vertical", 0.0f);
+    }
 }
